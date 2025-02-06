b65010a74749587ec7831d03a85a310f2fe80c43
ziel-main <fazriel.keysha@gmail.com>
2025-02-06 19:33:21 +0700

    bilangan ganjil

---
+++ JS/main.js
@@ -0,0 +1,35 @@
+// alert("Siapa yang suruh lu kesini anjing");
+// prompt("namina saha a'?");
+// console.log("halo anjing");
+
+// const nama = "kontol maniak";
+// let beratBadan = 18;
+
+// beratBadan = beratBadan + 1;
+
+// console.log(nama);
+// console.log(beratBadan);
+
+// let pekerjaan = "admin";
+
+// pekerjaan = "developer";
+
+// console.log(pekerjaan);
+
+// let tinggiBadan = 170;
+
+// tinggiBadan = tinggiBadan + 1;
+
+// console.log(tinggiBadan);
+
+// console.log(typeof nama);
+// console.log(typeof beratBadan);
+// console.log(typeof tinggiBadan);
+// console.log(typeof pekerjaan);
+
+let angka = 50;
+for (i = 1; i <= 50; i = i + 1) {
+if (i % 2 == 1) {
+    console.log(i + ' adalah bilangan ganjil');
+  }
+}
