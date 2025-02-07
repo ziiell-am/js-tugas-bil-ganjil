8b028033dc3b5c7f6d06e21072571929a15b6486
ziel-main <fazriel.keysha@gmail.com>
2025-02-07 20:06:27 +0700

    tugas 2 ruang belajar

--- JS/total bilangan.js
+++ JS/total bilangan.js
@@ -1,5 +1,13 @@
-let angka = 0;
-for (i = 1; i <= 50; i++) {
-    angka += i
+// let angka = 0;
+// for (i = 1; i <= 50; i++) {
+    // angka += i
+// }
+// console.log(angka);
+
+// tugas 2 reverse
+//Tugas 2 js
+
+function reverseKata(word) {
+    return word.split('').reverse().join('');
 }
-console.log(angka);
+console.log(reverseKata('xFyeeZ'));
